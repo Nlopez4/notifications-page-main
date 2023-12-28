40cdea8936dc18efd1a0192cd8b4430806fd857e
nataliel2514 <natalielopez2514@gmail.com>
2023-12-28 15:03:13 -0600

    add click all

--- app/script.js
+++ app/script.js
@@ -1,21 +1,36 @@
 const notificationsContainer = document.querySelectorAll('.notification-container');
 const notifications = document.querySelectorAll('h3');
+const marked = document.querySelector('#marked');
+
 
 function clickNotice(){
     notificationsContainer.forEach(function(notfication){
         notfication.addEventListener("click", function(e){
             let clicked = e.target;
-            console.log(clicked)
             e.preventDefault();
             notfication.style.backgroundColor = 'white';
             notifications.forEach(function(element){
                 if (element === clicked){
                     let afterElement = getComputedStyle(element)
                     element.style.setProperty('--after-color', 'white');
+                } else {
+                    return;
                 }
             })
         });
     })
 }
-clickNotice()
+clickNotice();
+
+function clickAll(){
+    marked.addEventListener('click', function(e){
+        e.preventDefault();
+        notificationsContainer.forEach(function(el){
+            el.classList.add('read');
+        });
+    })
+}
+clickAll();
+
+
 
