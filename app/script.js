c4d7996bde12a29909678d96f1ce14e3b9247e67
Natalie Lopez <Natalie.Lopez@abelsontaylor.com>
2024-01-02 16:08:32 -0600

    user info added

--- app/script.js
+++ app/script.js
@@ -11,14 +11,36 @@ renderData();
 
 function createElements(data){
     const {notifications} = data;
-    for (let i of notifications){
-        console.log(notifications)
-        let container = document.createElement('div');
-        container.className = 'notification-container'
-        container.innerHTML = i.firstName + ' ' + i.lastName;
-        mainContainer.appendChild(container)
+    for (let notification of notifications){
+        const notificationContainer = document.createElement('div');
+        const leftContainer = document.createElement('div');
+        const userImage = document.createElement('img');
+        const rightContainer = document.createElement('div');
+        const notificationTag = document.createElement('a');
+        const h3 = document.createElement('h3');
+        const time = document.createElement('p');
+
+        notificationContainer.className = 'notification-container';
+        leftContainer.className = 'container-left';
+        rightContainer.className = 'container-right';
+        userImage.src = notification.photo;
+        notificationTag.className = 'notifications';
+        h3.innerHTML = notification.firstName + ' ' + notification.lastName + ' ' + notification.notification;
+        time.className = 'time';
+        time.innerHTML = notification.time;
+        
+        mainContainer.appendChild(notificationContainer);
+        notificationContainer.appendChild(leftContainer);
+        notificationContainer.appendChild(rightContainer);
+        leftContainer.appendChild(userImage);
+        rightContainer.appendChild(notificationTag);
+        notificationTag.appendChild(h3);
+        rightContainer.appendChild(time);
+
     }
 }
+
+
 
 
 
